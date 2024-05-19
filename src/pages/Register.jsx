fc6696beb48855e6dc4b1efd06d5d5e3c5408652
LeoDomGit <leodomsolar@gmail.com>
2024-05-19 15:31:52 +0700

    Commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -116,7 +116,7 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Flappy Bird
+           Bird
           </Typography>
           <Box  component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
